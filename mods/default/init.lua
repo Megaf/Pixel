7aa9c75620e281a16fa53c11bc68e2d0713ba4f2
Megaf <mmegaf@gmail.com>
2014-11-30 22:02:36 +0000

    Added a function to set your local area as "home" and them being able to teleport to there, code based on sethome mod for minetest.

--- mods/default/init.lua
+++ mods/default/init.lua
@@ -17,7 +17,7 @@ minetest.register_item(":", {
 			oddly_breakable_by_hand = {times={[1]=0.05,[2]=0.05,[3]=0.05}, uses=0, maxlevel=4},
 			}
 		}
-}),
+})
 
 minetest.register_node("default:dirt_with_grass", {
 	description = "Dirt with grass",
@@ -53,13 +53,72 @@ minetest.register_alias("mapgen_cobble", "default:dirt_with_grass")
 minetest.register_alias("mapgen_dirt_with_grass", "default:dirt_with_grass")
 minetest.register_alias("mapgen_stone", "default:dirt_with_grass")
 minetest.register_alias("mapgen_dirt", "default:dirt_with_grass")
+local homes_file = minetest.get_worldpath() .. "/homes"
+local homepos = {}
+local function loadhomes()
+	local input = io.open(homes_file, "r")
+	if input then
+		repeat
+			local x = input:read("*n")
+			if x == nil then
+				break
+			end
+			local y = input:read("*n")
+			local z = input:read("*n")
+			local name = input:read("*l")
+			homepos[name:sub(2)] = {x = x, y = y, z = z}
+		until input:read(0) == nil
+		io.close(input)
+	else
+		homepos = {}
+	end
+end
+loadhomes()
+local changed = false
+minetest.register_chatcommand("home", {
+	description = "Type /home to teleport yourself to your land.",
+	privs = {interact=true},
+	func = function(name)
+		local player = minetest.get_player_by_name(name)
+		if player == nil then
+			return false
+		end
+		if homepos[player:get_player_name()] then
+			player:setpos(homepos[player:get_player_name()])
+			minetest.chat_send_player(name, "You have been teleported to your area.")
+		else
+			minetest.chat_send_player(name, "Type /sethome to save the place where you are, allowing you to teleport back to it later on.")
+		end
+	end,
+})
+minetest.register_chatcommand("sethome", {
+		description = "Type /sethome to save the place where you are, allowing you to teleport back to it later on.",
+		privs = {interact=true},
+		func = function(name)
+		local player = minetest.get_player_by_name(name)
+		local pos = player:getpos()
+		homepos[player:get_player_name()] = pos
+		minetest.chat_send_player(name, "Your position have been recorded.")
+		changed = true
+		if changed then
+			local output = io.open(homes_file, "w")
+			for i, v in pairs(homepos) do
+				output:write(v.x.." "..v.y.." "..v.z.." "..i.."\n")
+			end
+			io.close(output)
+			changed = false
+		end
+	end,
+})
 
---dofile(minetest.get_modpath("default").."/mapgen.lua")
+
+--dofile(minetest.get_modpath("default").."/file.lua")
 --[[ minetest.register_on_joinplayer(function(player)
 	local cb = function(player)
-		minetest.chat_send_player(player:get_player_name(), "This is the [minimal] \"Minimal Development Test\" game. Use [minetest_game] for the real thing.")
+		minetest.chat_send_player(player:get_player_name(), "Text")
 	end
 	minetest.after(2.0, cb, player)
 end) ]]
+--minetest.register_privilege("priv", "description")
 
 
