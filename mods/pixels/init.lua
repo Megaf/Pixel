1b819402ffc1aafe8b301e50ab18771f4bf5be1a
Megaf <mmegaf@gmail.com>
2014-12-03 13:06:15 +0000

    Updating code.

--- mods/pixels/init.lua
+++ mods/pixels/init.lua
@@ -22,7 +22,7 @@ for _, row in ipairs(pixels.colors) do
 	minetest.register_node("pixels:"..name, {
 		description = name.." Pixel",
 		tiles = {"pixel_"..name..".png"},
-		groups = {cracky=3},
+		groups = {cracky=2},
 		stack_max = 10000,
 		light_source = 4,
 		sunlight_propagates = true,
@@ -33,7 +33,7 @@ for _, row in ipairs(pixels.colors) do
 	minetest.register_node("pixels:"..name.."_framed", {
 		description = "Framed (black border)"..name.." Pixel",
 		tiles = {"pixel_"..name..".png^pixel_frame.png"},
-		groups = {cracky=3},
+		groups = {cracky=2},
 		stack_max = 10000,
 		light_source = 4,
 		sunlight_propagates = true,
